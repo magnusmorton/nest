ffd46cfb63e93af134136d955247ed04d9ea5f37
Magnus Morton <magnusmorton@gmail.com>
2012-01-25 02:14:18 +0000

    test passing

--- nest/loop.py
+++ nest/loop.py
@@ -4,3 +4,17 @@ def containsLoop(ast):
     loopVistor = LoopVistor()
     loopVistor.visit(ast)
     return False
+    
+    
+class LoopVisitor(ast.NodeVisitor):
+    
+    def __init__(self):
+        super(LoopVisitor, self).__init__()
+        self._loops_found = 0
+    
+    @property    
+    def loops_found(self):
+        return self._loops_found
+        
+    def visit_For(self, node):
+        self._loops_found += 1
--- nest/tests/test_loop_extract.py
+++ nest/tests/test_loop_extract.py
@@ -3,25 +3,33 @@ import ast
 import unittest
 from nose import with_setup
 
-LOOP = """for i in [1,2,3]:
+SIMPLE_LOOP = """for i in [1,2,3]:
     a = 1
 """
+
+NON_LOOP_STATEMENT = "print(a)"
 
 """ Ignore these first tests.  They were included when I was trying to figure out what to do.
 """
 def setup():
     global simple_loop_ast 
-    simple_loop_ast = ast.parse(LOOP)
+    simple_loop_ast = ast.parse(SIMPLE_LOOP)
 
 def testFindLoop():
     assert nest.loop.containsLoop(simple_loop_as), "loop not found"
     
 class LoopVistitorTests(unittest.TestCase):
     def setUp(self):
-        self.trivial_loop_ast = ast.parse(LOOP)
+        self.trivial_loop_ast = ast.parse(SIMPLE_LOOP)
         
         
     def test_1_loop_found(self):
         loop_visitor = nest.loop.LoopVisitor()
-        loop_visitor.visit(trivial_loop_ast)
-        self.assertEqual(loop_vistor.loops_found(), 1, "Wrong number of loops found")
+        loop_visitor.visit(self.trivial_loop_ast)
+        self.assertEqual(loop_visitor.loops_found, 1, "Wrong number of loops found")
+        
+    def test_no_loops_found(self):
+        loop_visitor = nest.loop.LoopVisitor()
+        loop_visitor.visit(ast.parse(NON_LOOP_STATEMENT))
+        self.assertEqual(loop_visitor.loops_found, 0, "loops found when not present")
+        
