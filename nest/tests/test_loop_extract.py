710dc42ef6a59450655579dc25c468c92b33d7c3
Magnus Morton <magnusmorton@gmail.com>
2012-01-25 02:26:07 +0000

    added test for 2 loops

--- nest/tests/test_loop_extract.py
+++ nest/tests/test_loop_extract.py
@@ -7,6 +7,15 @@ SIMPLE_LOOP = """for i in [1,2,3]:
     a = 1
 """
 
+TWO_LOOPS = """for i in [1,2,3]:
+    a = 1
+
+for num in [1,2,3]:
+    a = num
+
+"""
+
+
 NON_LOOP_STATEMENT = "print(a)"
 
 """ Ignore these first tests.  They were included when I was trying to figure out what to do.
@@ -24,9 +33,14 @@ class LoopVistitorTests(unittest.TestCase):
         
     def test_1_loop_found(self):
         self.loop_visitor.visit(ast.parse(SIMPLE_LOOP))
-        self.assertEqual(self.loop_visitor.loops_found, 1, "Wrong number of loops found")
+        self.assertEqual(self.loop_visitor.loops_found, 1, "Only one loop should be found")
         
     def test_no_loops_found(self):
         self.loop_visitor.visit(ast.parse(NON_LOOP_STATEMENT))
         self.assertEqual(self.loop_visitor.loops_found, 0, "loops found when not present")
+        
+    def test_two_loops_found(self):
+        self.loop_visitor.visit(ast.parse(TWO_LOOPS))
+        self.assertEqual(self.loop_visitor.loops_found, 2, "Wrong number of loops found")
+        
         
