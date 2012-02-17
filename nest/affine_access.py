7cdb837e8c9ba7f1f4b3297fdc21c7f34fb5904e
Magnus Morton <magnusmorton@gmail.com>
2012-02-17 14:55:40 +0000

    added equality support

--- nest/affine_access.py
+++ nest/affine_access.py
@@ -6,16 +6,19 @@ Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 """
 
 class AffineAccess():
-    
+
     def __init__(self):
         self._params = {}
-    
+
     def add_param(self, name, coeff=1):
         self._params[name] = coeff
-           
+
     @property
     def params(self):
         return self._params
 
     def get_coeff(self, param):
     	return self._params[param]
+
+    def __eq__(self, other):
+        return self._params == other._params
--- nest/tests/test_access.py
+++ nest/tests/test_access.py
@@ -29,20 +29,35 @@ class TestAccess(unittest.TestCase):
 
     def test_explicit_coeff_extractable(self):
         self.access.add_param("i", 3)
-        self.assertEqual(self.access.get_coeff("i"), 3, "coeffecient of i is not 3") 
+        self.assertEqual(self.access.get_coeff("i"), 3, "coeffecient of i is not 3")
+
+    def test_eq_true_when_equal(self):
+        other_access = AffineAccess()
+        self.access.add_param("i")
+        other_access.add_param("i")
+        self.assertEqual(self.access, other_access, "accesses not equal")
+
+    def test_eq_false_when_not_equal(self):
+        other_access = AffineAccess()
+        self.access.add_param("i")
+        other_access.add_param("j")
+        self.assertNotEqual(self.access, other_access, "accesses equal")
 	        
     def add_params(self, *params):
         for param in params:
             self.access.add_param(param)
 
 
-#class TestSubscriptVisitor(unittest.TestCase):
-#   
-#    def setUp(self):
-#        pass
-#
-#    def simple_access_detected(self):
-#        
+class TestSubscriptVisitor(unittest.TestCase):
+    
+    SIMPLE_ACCESS = "[i]"
+    def setUp(self):
+        pass
+
+    #def simple_access_detected(self):
+        #visitor = SubscriptVisistor()
+        #visitor.visit(ast.parse(SIMPLE_ACCESS))
+        #assertEqual
 
 	
     
