9d3a386030ee4ff2ebd5170866675667b56c0a51
Magnus Morton <magnusmorton@gmail.com>
2012-03-08 20:17:31 +0000

    can now extract negative coefficient

--- nest/affine_access.py
+++ nest/affine_access.py
@@ -33,38 +33,55 @@ class SubscriptVisitor(ast.NodeVisitor):
         self._access = AffineAccess()
         self._context = 0
         self._foundID = None
+        self._context_stack = []
 
     @property
     def access(self):
         return self._access
         
     def visit_Name(self, node):
+        context = None
+        if self._context_stack:
+            context = self._context_stack[len(self._context_stack)-1]
         self._access.add_param(node.id)
-        if self._context == SubscriptVisitor.MULT:
+        if context == SubscriptVisitor.MULT:
             self._foundID = node.id
-        if self._context == SubscriptVisitor.NEG or self._context == SubscriptVisitor.SUB: 
+        if context == SubscriptVisitor.NEG or self._context == SubscriptVisitor.SUB: 
             self._foundID = node.id
             self._found_const = -1
         
     def visit_Num(self, node):
-        self._found_const = node.n
+        if self._context_stack[len(self._context_stack)-1] == SubscriptVisitor.NEG:
+            print("HELLOOOOOO")
+            self._found_const = -node.n
+        else:
+            self._found_const = node.n
+        print(self._found_const)
         
     '''This will blow up when sym constants are involved!!!! (Right now, constants too)'''    
     def visit_BinOp(self, node):
         if isinstance(node.op,ast.Mult):
+            self._context_stack.append(SubscriptVisitor.MULT)
             self._context = SubscriptVisitor.MULT
+            print("before HELLOOOOO")
         elif isinstance(node.op, ast.Sub):
+            self._context_stack.append(SubscriptVisitor.SUB)
             self._context = SubscriptVisitor.SUB
         else:
+            self._context_stack.append(SubscriptVisitor.GENERIC)
             self._context = SubscriptVisitor.GENERIC
         self.generic_visit(node)
+        self._context_stack.pop()
         if self._foundID:
+            print(self._found_const)
             self._access.add_param(self._foundID, self._found_const)
             
     def visit_UnaryOp(self, node):
         if isinstance(node.op, ast.USub):
+            self._context_stack.append(SubscriptVisitor.NEG)
             self._context = SubscriptVisitor.NEG
         self.generic_visit(node)
+        self._context_stack.pop()
         if self._foundID:
             self._access.add_param(self._foundID, self._found_const)
         
