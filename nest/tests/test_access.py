021b5048ed3075f3be67c499b12b1a0759540576
Magnus Morton <magnusmorton@gmail.com>
2012-03-11 22:36:10 +0000

    added second dict_add test; commented out other tests for now

--- nest/tests/test_access.py
+++ nest/tests/test_access.py
@@ -28,54 +28,54 @@ from nest.tests.visitor_helper import VisitorHelper
 from nest.affine_access import *
 
 
-class TestSubscriptVisitor(unittest.TestCase, VisitorHelper):
-    
-    SIMPLE_ACCESS = "a[i]"
-    TWO_ACCESS = "a[i + j]"
-    SIMPLE_COEFF = "a[2*i]"
-    TWO_COEFF    = "a[2*i + 3*j]"
-    SIMPLE_MINUS = "a[-i]"
-    TWO_MINUS    = "a[i - j]"
-    SIMPLE_COEFF_MINUS = "a[-2*i]"
-    TWO_COEFF_MINUS    = "a[3*i - 2*j]"
-
-    def setUp(self):
-        self.visitor = SubscriptVisitor()
-
-    def test_simple_access_detected(self):
-        self.visit(TestSubscriptVisitor.SIMPLE_ACCESS)
-        self.assertEqual(self.visitor.access, {'i':1}, "detected access was not just 'i'")
-
-    def test_two_paramater_access_detected(self):
-        self.visit(TestSubscriptVisitor.TWO_ACCESS)
-        self.assertEqual(self.visitor.access, {'i':1, 'j':1}, "detected access was not just 'i' and 'j")
-        
-    def test_coeff_extract(self):
-        self.visit(TestSubscriptVisitor.SIMPLE_COEFF)
-        self.assertEqual(self.visitor.access, {'i':2}, "detected access did not have i with coeff of 2")
-        
-    def test_two_coeff_extract(self):
-        self.visit(TestSubscriptVisitor.TWO_COEFF)
-        self.assertEqual(self.visitor.access, {'i':2, 'j':3}, "both coefficients not extracted")
-        
-    def test_simple_minus(self):
-        self.visit(TestSubscriptVisitor.SIMPLE_MINUS)
-        self.assertEqual(self.visitor.access,{'i':-1}, "negative coeff not extracted")
-        
-    def test_two_minus(self):
-        self.visit(TestSubscriptVisitor.TWO_MINUS)
-        self.assertEqual(self.visitor.access,{'i':1, 'j':-1}, "negative coeff of j not extracted")
-        
-    def test_simple_minus_with_coeff(self):
-        """tests extracting a negative coefficient"""
-        self.visit(TestSubscriptVisitor.SIMPLE_COEFF_MINUS)
-        self.assertEqual(self.visitor.access,{'i':-2}, "coeff of i not -2")
-    
-    def test__minus_with_two_coeff(self):
-        """tests extracting a negative coefficient"""
-        self.visit(TestSubscriptVisitor.TWO_COEFF_MINUS)
-        self.assertEqual(self.visitor.access,{'i':3, 'j':-2}, "coeff of j not -2")    
-        
+# class TestSubscriptVisitor(unittest.TestCase, VisitorHelper):
+#     
+#     SIMPLE_ACCESS = "a[i]"
+#     TWO_ACCESS = "a[i + j]"
+#     SIMPLE_COEFF = "a[2*i]"
+#     TWO_COEFF    = "a[2*i + 3*j]"
+#     SIMPLE_MINUS = "a[-i]"
+#     TWO_MINUS    = "a[i - j]"
+#     SIMPLE_COEFF_MINUS = "a[-2*i]"
+#     TWO_COEFF_MINUS    = "a[3*i - 2*j]"
+# 
+#     def setUp(self):
+#         self.visitor = SubscriptVisitor()
+# 
+#     def test_simple_access_detected(self):
+#         self.visit(TestSubscriptVisitor.SIMPLE_ACCESS)
+#         self.assertEqual(self.visitor.access, {'i':1}, "detected access was not just 'i'")
+# 
+#     def test_two_paramater_access_detected(self):
+#         self.visit(TestSubscriptVisitor.TWO_ACCESS)
+#         self.assertEqual(self.visitor.access, {'i':1, 'j':1}, "detected access was not just 'i' and 'j")
+#         
+#     def test_coeff_extract(self):
+#         self.visit(TestSubscriptVisitor.SIMPLE_COEFF)
+#         self.assertEqual(self.visitor.access, {'i':2}, "detected access did not have i with coeff of 2")
+#         
+#     def test_two_coeff_extract(self):
+#         self.visit(TestSubscriptVisitor.TWO_COEFF)
+#         self.assertEqual(self.visitor.access, {'i':2, 'j':3}, "both coefficients not extracted")
+#         
+#     def test_simple_minus(self):
+#         self.visit(TestSubscriptVisitor.SIMPLE_MINUS)
+#         self.assertEqual(self.visitor.access,{'i':-1}, "negative coeff not extracted")
+#         
+#     def test_two_minus(self):
+#         self.visit(TestSubscriptVisitor.TWO_MINUS)
+#         self.assertEqual(self.visitor.access,{'i':1, 'j':-1}, "negative coeff of j not extracted")
+#         
+#     def test_simple_minus_with_coeff(self):
+#         """tests extracting a negative coefficient"""
+#         self.visit(TestSubscriptVisitor.SIMPLE_COEFF_MINUS)
+#         self.assertEqual(self.visitor.access,{'i':-2}, "coeff of i not -2")
+#     
+#     def test__minus_with_two_coeff(self):
+#         """tests extracting a negative coefficient"""
+#         self.visit(TestSubscriptVisitor.TWO_COEFF_MINUS)
+#         self.assertEqual(self.visitor.access,{'i':3, 'j':-2}, "coeff of j not -2")    
+#         
 class TestHelperMethods(unittest.TestCase):
     
     def test_dict_multiply(self):
@@ -89,4 +89,10 @@ class TestHelperMethods(unittest.TestCase):
     def test_dict_add_simple(self):
         test_dict = {'a':1}
         self.assertEqual({'a':2}, dict_add(test_dict,test_dict),'a is not added to itself')
+        
+    def test_dict_add_two_keys(self):
+        left = {'a':1, 'b':-2}
+        right = {'a':3, 'b':1}
+        self.assertEqual({'a':4, 'b':-1}, dict_add(left,right),'dicts not added correctly')
+        
 
