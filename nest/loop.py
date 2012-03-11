bf0f57794c7433fc751259338773c5aed98566c9
Magnus Morton <magnusmorton@gmail.com>
2012-03-11 19:38:07 +0000

    Added License heasers

--- nest/loop.py
+++ nest/loop.py
@@ -1,3 +1,21 @@
+'''
+(c) Copyright 2012 Magnus Morton. All Rights Reserved. 
+
+This file is part of Nest.
+
+Nest is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Nest is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Nest.  If not, see <http://www.gnu.org/licenses/>.
+'''
 import ast
 
 
