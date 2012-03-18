5a85f0a653cfee8d328183fdd6aedd70d8ac3df0
Magnus Morton <magnusmorton@gmail.com>
2012-03-18 20:32:44 +0000

    added stub transformer class

---
+++ nest/transformer.py
@@ -0,0 +1,29 @@
+'''
+(c) Copyright 2012 Magnus Morton.
+
+This file is part of Nest.
+
+Nest is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Nest is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Nest.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+class Transformer(object):
+
+    def __init__(self, abstract_tree, loops):
+        self._visitor = abstract_tree
+        self._loops = loops
+
+
+    
+
+
