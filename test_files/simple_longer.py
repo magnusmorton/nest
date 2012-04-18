7880215796f33236cd9a87a51f205dbee29a7cac
Magnus Morton <magnusmorton@gmail.com>
2012-04-18 15:46:15 +0100

    test changes

---
+++ test_files/simple_longer.py
@@ -0,0 +1,8 @@
+
+def loop():
+    a = [0]*1000000
+    for i in range(1000000):
+        a[i] = 2*3 + 45**4
+    print(a)
+
+loop()
--- test_files/simple_multidim.py
+++ test_files/simple_multidim.py
@@ -1,5 +1,5 @@
-a = [[0] * 1000]*1000
+a = [[0] * 10000]*10000
 
-for i in range(1000):
-    for j in range(1000):
+for i in range(10000):
+    for j in range(10000):
         a[i][j] = 45 + 12 + 34**2
